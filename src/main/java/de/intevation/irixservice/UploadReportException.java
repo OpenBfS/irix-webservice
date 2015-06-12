324f144347bae66aa39c7c8fac793b5d9480c7f8
Tom Gottfried <tom@intevation.de>
2015-06-12 12:58:11 +0200

    Add missing header.

--- src/main/java/de/intevation/irixservice/UploadReportException.java
+++ src/main/java/de/intevation/irixservice/UploadReportException.java
@@ -1,3 +1,11 @@
+/* Copyright (C) 2015 by Bundesamt fuer Strahlenschutz
+ * Software engineering by Intevation GmbH
+ *
+ * This file is Free Software under the GNU GPL (v>=3)
+ * and comes with ABSOLUTELY NO WARRANTY!
+ * See LICENSE.txt for details.
+ */
+
 package de.intevation.irixservice;
 
 public class UploadReportException extends Exception {
